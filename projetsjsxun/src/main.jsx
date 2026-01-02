ab211ca791c55ab08a33cf46eb60a4c9f024904f
youngfresnel <youngfresnel696@gmail.com>
2026-01-02 11:11:41 +0100

    Vercel

--- projetsjsxun/src/main.jsx
+++ projetsjsxun/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './index.jsx'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 createRoot(document.getElementById('root')).render(
